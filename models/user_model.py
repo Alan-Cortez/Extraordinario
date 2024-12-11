ff6760104d285b3a4ed26238906fa48ae405b50f
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-12-11 16:44:31 -0600

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -15,27 +15,27 @@ class UserModel:
 
     def obtener_usuarios(self):
         self.verificar_conexion()
-        cursor = self.con.cursor()
-        cursor.execute("SELECT * FROM tst0_usuarios ORDER BY Id_Usuario DESC")
-        return cursor.fetchall()
+        with self.con.cursor() as cursor:
+            cursor.execute("SELECT * FROM tst0_usuarios ORDER BY Id_Usuario DESC")
+            return cursor.fetchall()
 
     def guardar_usuario(self, usuario, contrasena):
         self.verificar_conexion()
-        cursor = self.con.cursor()
-        sql = "INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)"
-        cursor.execute(sql, (usuario, contrasena))
-        self.con.commit()
+        with self.con.cursor() as cursor:
+            sql = "INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)"
+            cursor.execute(sql, (usuario, contrasena))
+            self.con.commit()
 
     def actualizar_usuario(self, id, usuario, contrasena):
         self.verificar_conexion()
-        cursor = self.con.cursor()
-        sql = "UPDATE tst0_usuarios SET Nombre_Usuario = %s, Contrasena = %s WHERE Id_Usuario = %s"
-        cursor.execute(sql, (usuario, contrasena, id))
-        self.con.commit()
+        with self.con.cursor() as cursor:
+            sql = "UPDATE tst0_usuarios SET Nombre_Usuario = %s, Contrasena = %s WHERE Id_Usuario = %s"
+            cursor.execute(sql, (usuario, contrasena, id))
+            self.con.commit()
 
     def eliminar_usuario(self, id):
         self.verificar_conexion()
-        cursor = self.con.cursor()
-        sql = "DELETE FROM tst0_usuarios WHERE Id_Usuario = %s"
-        cursor.execute(sql, (id,))
-        self.con.commit()
+        with self.con.cursor() as cursor:
+            sql = "DELETE FROM tst0_usuarios WHERE Id_Usuario = %s"
+            cursor.execute(sql, (id,))
+            self.con.commit()
